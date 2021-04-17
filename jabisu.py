154400c7dfa1c6d6a43b8bec20012513c189504b
Mohammad Uvaiz <mohammeduvaiz0786@gmail.com>
2021-04-17 14:49:45 +0530

    introduce wishing function

--- jabisu.py
+++ jabisu.py
@@ -1,7 +1,7 @@
-# WITH THIS WHOLE CODE OUR JABISU IS READY TO SPEAK
+
 
 import pyttsx3                                # A python library that will help us to convert text to speech
-
+import datetime
 
 engine = pyttsx3.init('sapi5')                # Microsoft developed speech API Helps in synthesis and recognition of voice.
 voices = engine.getProperty('voices')
@@ -14,7 +14,17 @@ def speak(audio):                             # we will write our speak() functi
     engine.say(audio)     
     engine.runAndWait()                       # without these function we can't hear the audio 
 
-if __name__=="__main__":                      # calling speak function inside my main function
 
+def wishme():                                 # declaring wishme function
+    hour = int(datetime.datetime.now().hour)  
+    if hour>=0 and hour<12:
+        speak(" good morning")
+    elif hour>=12 and hour<18:
+        speak(" good afternoon")
+    else:
+        speak(" good evening")
+
+if __name__=="__main__":                      # calling speak function inside my main function
+    wishme()
     speak(" i am jabisu how can i help you ") # inside this speak function what u write is speaks by our AI
   
